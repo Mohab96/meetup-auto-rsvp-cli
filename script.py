62651b9fd7249690e1c0cb2486c5226494f33da1
Mohab Yaser <mohabkhaleefa540@gmail.com>
2024-04-01 02:13:07 +0200

    Added the initial structure of the code + login function

---
+++ script.py
@@ -0,0 +1,83 @@
+import os
+import time
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.remote.webelement import WebElement
+
+
+def get_element(browser: webdriver.Chrome, by: By, val: str) -> WebElement:
+    time.sleep(5)
+    # WebDriverWait(browser, 20).until(EC.presence_of_element_located((by, val)))
+
+    return browser.find_element(by, val)
+
+
+def get_elements(browser: webdriver.Chrome, by: By, val: str) -> list[WebElement]:
+    time.sleep(5)
+    # WebDriverWait(browser, 20).until(EC.presence_of_element_located((by, val)))
+
+    return browser.find_elements(by, val)
+
+
+def login(email: str, password: str) -> None:
+    browser.get(
+        'https://www.meetup.com/login/?returnUri=https://www.meetup.com/groups/')
+
+    # Entering email
+    get_element(browser, By.XPATH, '//*[@id="email"]').send_keys(email)
+
+    # Entering password
+    get_element(browser, By.XPATH,
+                '//*[@id="current-password"]').send_keys(password)
+
+    # Clicking the login button
+    get_element(browser, By.XPATH,
+                '//*[@id="main"]/div/div/div/section/form/div[2]/div[1]/button').click()
+
+    time.sleep(5)
+
+
+def get_groups(browser: webdriver.Chrome) -> list[str]:
+    pass
+
+
+def not_RSVPed(browser: webdriver.Chrome, counter) -> bool:
+    pass
+
+
+def get_events(browser: webdriver.Chrome, group_urls: list[str]) -> list[str]:
+    pass
+
+
+def rsvp_to_events(browser: webdriver.Chrome, events: list[str]) -> None:
+    pass
+
+
+def success(browser: webdriver.Chrome) -> None:
+    pass
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    email = os.getenv('EMAIL')
+    password = os.getenv('PASSWORD')
+
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    browser = webdriver.Chrome(options=options)
+    browser.maximize_window()
+
+    login(email, password)
+
+    groups = get_groups(browser)
+
+    events = get_events(browser, groups)
+
+    rsvp_to_events(browser, events)
+
+    success(browser)
